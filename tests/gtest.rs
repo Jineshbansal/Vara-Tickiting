707e4f23d7d716afac72b5847ef180175474d925
lla-dane <abhinavagarwalla6@gmail.com>
2025-01-11 00:28:56 +0530

    payments

--- tests/gtest.rs
+++ tests/gtest.rs
@@ -1,14 +1,20 @@
-use sails_rs::{calls::*, gtest::{calls::*, System}};
+use sails_rs::{
+    calls::*,
+    gtest::{calls::*, System},
+};
 
 use vara_ticket_client::traits::*;
 
 const ACTOR_ID: u64 = 42;
+const USER_ID: [u64; 2] = [12, 13];
 
 #[tokio::test]
-async fn do_something_works() {
+async fn do_something() {
     let system = System::new();
     system.init_logger();
     system.mint_to(ACTOR_ID, 100_000_000_000_000);
+    system.mint_to(USER_ID[0], 100_000_000_000_000);
+    system.mint_to(USER_ID[1], 100_000_000_000_000);
 
     let remoting = GTestRemoting::new(system, ACTOR_ID.into());
     remoting.system().init_logger();
@@ -19,7 +25,7 @@ async fn do_something_works() {
     let program_factory = vara_ticket_client::VaraTicketFactory::new(remoting.clone());
 
     let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
+        .new("EZZ".to_string(), "EZZ".to_string(), 18)
         .send_recv(program_code_id, b"salt")
         .await
         .unwrap();
@@ -27,41 +33,11 @@ async fn do_something_works() {
     let mut service_client = vara_ticket_client::VaraTicket::new(remoting.clone());
 
     let result = service_client
-        .do_something() // Call service's method (see app/src/lib.rs:14)
+        .name("EZZ".to_string(), "EZZ".to_string(), 18) // Call service's method (see app/src/lib.rs:14)
         .send_recv(program_id)
         .await
         .unwrap();
 
-    assert_eq!(result, "Hello from VaraTicket!".to_string());
-}
-
-#[tokio::test]
-async fn get_something_works() {
-    let system = System::new();
-    system.init_logger();
-    system.mint_to(ACTOR_ID, 100_000_000_000_000);
-
-    let remoting = GTestRemoting::new(system, ACTOR_ID.into());
-    remoting.system().init_logger();
-
-    // Submit program code into the system
-    let program_code_id = remoting.system().submit_code(vara_ticket::WASM_BINARY);
-
-    let program_factory = vara_ticket_client::VaraTicketFactory::new(remoting.clone());
-
-    let program_id = program_factory
-        .new() // Call program's constructor (see app/src/lib.rs:29)
-        .send_recv(program_code_id, b"salt")
-        .await
-        .unwrap();
-
-    let service_client = vara_ticket_client::VaraTicket::new(remoting.clone());
-
-    let result = service_client
-        .get_something() // Call service's query (see app/src/lib.rs:19)
-        .recv(program_id)
-        .await
-        .unwrap();
-
-    assert_eq!(result, "Hello from VaraTicket!".to_string());
+    // assert_eq!(result, "Hello from VaraTicket!".to_string())
+    println!("{result}");
 }
